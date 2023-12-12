0cc6fea9636d77c1c66745714280e2d1193873c7
ryanfarber <32393930+ryanfarber@users.noreply.github.com>
2023-12-12 13:02:19 -0300

    update

--- src/src.js
+++ src/src.js
@@ -6,22 +6,26 @@ const _ = require("underscore")
 const fs = require("fs")
 const fsp = require("fs").promises
 const Logger = require("@ryanforever/logger").v2
-const logger = new Logger(__filename, {debug: true})
-const voiceCache = require("../cache/voices.json")
+
+// const voiceCache = require("../cache/voices.json")
 const dateformat = require("dateformat")
 const templates = require("./templates.js")
-
-
+const path = require("path")
+const Cache = require("@ryanforever/local-database")
 
 
 class Elevenlabs {
 	constructor(config = {}) {
 
 		const key = config.apiKey || config.key || config.token
-
+		const savePath = config.savePath || "../output"
+		const cache = new Cache(path.join(__dirname, "../cache/cache.json"))
+		const logger = new Logger("elevenlabs", {debug: config.debug ?? false})
 
 		axios.defaults.baseURL = "https://api.elevenlabs.io/v1"
 		axios.defaults.headers.common["xi-api-key"] = key
+
+		const voiceCacheUpdateThreshold = 1000 * 30
 
 
 		this.syncVoices = async function() {
@@ -30,8 +34,16 @@ class Elevenlabs {
 			data = data.map(x => {
 				return _.pick(x, "name", "voice_id")
 			})
-			await fsp.writeFile("../cache/voices.json", JSON.stringify(data, null, 2))
+			// let obj = {
+			// 	updated: Date.now(),
+			// 	voices: data
+			// }
+			// let dir = path.join(__dirname, "../cache/voices.json")
+			// await fsp.writeFile(dir, JSON.stringify(obj, null, 2))
+			
+			cache.set("voices", data)
 			logger.debug("voices saved")
+			return data
 		}
 
 		/** get voices */
@@ -41,13 +53,31 @@ class Elevenlabs {
 			return data
 		}
 
+		this.getModels = async function() {
+			let res = await axios.get("/models")
+			let data = res.data
+			return data
+		}
+
+		this.syncModels = async function() {
+			let models = await this.getModels()
+			cache.set("models", models)
+		}
+
+		this.sync = async function() {
+			logger.debug(`syncing...`)
+			this.syncModels()
+			this.syncVoices()
+		}
+
 		/** generate speech */
 		this.generate = async function(text, config = {}) {
 			
-
 			if (!text) throw new Error("please input text")
 			let voice = config.voice || "pNInz6obpgDQGcFmaJgB"
 			let templateName = config.template
+			let model = config.model
+			let modelId = config.modelId
 			let num = config.num || 1
 			let voice_settings = {
 				stability: config.stability || 0,
@@ -61,8 +91,9 @@ class Elevenlabs {
 			}
 
 
-
-			let voiceData = voiceCache.find(x => x.name == voice || x.voice_id == voice)
+			let voices = cache.get("voices")
+			let voiceData = voices.find(x => x.name == voice || x.voice_id == voice)
+			if (!voiceData) throw new Error(`could not find voice "${voice}"`)
 			let voiceId = voiceData.voice_id
 			let voiceName = voiceData.name
 			let ts = dateformat(Date.now(), "yymmdd-HHMMss")
@@ -71,19 +102,25 @@ class Elevenlabs {
 
 			logger.debug(`generating speech using "${voiceData.name}"`)
 			logger.debug(`generating ${num} variation(s)`)
-			console.log(`\n"${text}"\n`)
+			// console.log(`\n"${text}"\n`)
 
 			if (!fs.existsSync("../output")) fs.mkdirSync("../output")
 
 			for (var i = 1; i <= num; i++) {
 
 				let filename = `${ts}_${voiceName}_${truncatedText}_${i}`
-				let filepath = `../output/${filename}.mp3`
+				let filepath = path.join(savePath, `${filename}.mp3`)
 
-				let res = await axios.post(`/text-to-speech/${voiceId}`, {text, voice_settings}, {
+				let res = await axios.post(`/text-to-speech/${voiceId}`, {
+					text, 
+					model_id: modelId,
+					voice_settings
+				}, {
 					responseType: "stream",
 				}).catch(err => {
-					logger.log(err)
+					// logger.log(err.data)
+					console.log(err.response)
+					throw new Error(err)
 				})
 				let data = res.data
 				
@@ -120,7 +157,7 @@ class Elevenlabs {
 
 		this.templates = templates
 		function truncateString(str, maxLength) {
-			str = str.replace(/\.|\?|\,/gi, "").trim()
+			str = str.replace(/\.|\?|\,|'|"|\!/gi, "").trim()
 			maxLength = maxLength || 20
 			if (str.length > maxLength) {
 		    // Trim the string to the maximum length
--- src/templates.js
+++ src/templates.js
@@ -1,6 +1,8 @@
 module.exports = new Map([
 
-	["oliver", {voice: "Oliver Tree V2", settings: {stability: .3, similarity_boost: .83}}],
-	["oliver2", {voice: "Oliver Tree V2", settings: {stability: .8, similarity_boost: .83}}]
+	["oliver", {voice: "Oliver Tree V2", settings: {stability: .2, similarity_boost: .83}}],
+	["oliver2", {voice: "Oliver Tree V2", settings: {stability: .8, similarity_boost: .83}}],
+	["monstertruck", {voice: "monster truck voice V1", settings: {stability: .5, similarity_boost: .83}} ],
+	["screen", {voice: "screen voice", settings: {stability: .3, similarity_boost: .83}} ],
 
 ])
